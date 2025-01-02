e007a5bc4f6352e897cc874d895753355cb2cbce
jeremy castillo taveras <jeremycastillo901@gmail.com>
2025-01-02 14:34:02 -0400

    Creando contexto

---
+++ ApiFinanzas/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using ApiFinanzas.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFinanzas.DAL
+{
+    public class Contexto : IdentityDbContext
+    {
+        public Contexto(DbContextOptions<Contexto> opt) : base(opt)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Presupuesto> Presupuestos { get; set; }
+        public DbSet<Transaccion> Transacciones { get; set; }
+        public DbSet<MetaFinanciera> MetaFinancieras { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
+    }
+}
