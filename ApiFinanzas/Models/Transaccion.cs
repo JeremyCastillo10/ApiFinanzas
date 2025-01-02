191ebfec0d894b50b0b2329866b50ecf9e0fe97f
jeremy castillo taveras <jeremycastillo901@gmail.com>
2025-01-02 14:33:20 -0400

    Creando modelo Transa

--- ApiFinanzas/Models/Transaccion.cs
+++ ApiFinanzas/Models/Transaccion.cs
@@ -1,7 +1,10 @@
-﻿namespace ApiFinanzas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFinanzas.Models
 {
     public class Transaccion
     {
+        [Key]
         public int Id { get; set; }
         public int UsuarioId { get; set; }
         public Usuario Usuario { get; set; }
