7263f827f094f90b0089d43390b23708b373c7c1
jeremy castillo taveras <jeremycastillo901@gmail.com>
2025-01-02 14:41:04 -0400

    Creando Controllador Categorias

--- ApiFinanzas/Controllers/CategoriaController.cs
+++ ApiFinanzas/Controllers/CategoriaController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiFinanzas.DAL;
+using ApiFinanzas.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Shared.DTOs.Categoria;
 
 namespace ApiFinanzas.Controllers
 {
@@ -7,5 +10,54 @@ namespace ApiFinanzas.Controllers
     [ApiController]
     public class CategoriaController : ControllerBase
     {
+        private readonly Contexto _db;
+        public CategoriaController(Contexto db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria_DTO>>> GetAllArticulos()
+        {
+            var articulos = await _db.Categorias
+                .Where(a => a.Eliminado == false)
+                .Select(a => new Categoria_DTO
+                {
+                    Id = a.Id,
+                    Nombre = a.Nombre,
+                })
+                .ToListAsync();
+
+            return Ok(articulos);
+        }
+        [HttpPost("Savecategoria")]
+
+        public async Task<ActionResult<int>> GuardarCategoria([FromBody] Categoria_DTO categoria_DTO)
+        {
+
+
+
+
+            if (categoria_DTO.Id > 0)
+            {
+                var categoria = await _db.Categorias.FindAsync(categoria_DTO.CategoriaId);
+                categoria.Nombre = categoria_DTO.Nombre;
+                categoria.FechaActualizacion = DateTime.Now;
+
+                await _db.SaveChangesAsync();
+
+            }
+            else
+            {
+                var categoria = new Categoria();
+
+                categoria.Nombre = categoria_DTO.Nombre;
+                categoria.FechaCreacion = DateTime.Now;
+                categoria.Eliminado = false;
+
+                _db.Categorias.Add(categoria);
+                await _db.SaveChangesAsync();
+            }
+            return 0;
+        }
     }
 }
