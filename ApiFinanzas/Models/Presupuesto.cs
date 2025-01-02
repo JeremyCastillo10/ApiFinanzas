036dddd2bf5d9dd1f26d6c71896df1919dcedd15
jeremy castillo taveras <jeremycastillo901@gmail.com>
2025-01-02 14:33:10 -0400

    Creando modelo presupuesto

---
+++ ApiFinanzas/Models/Presupuesto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFinanzas.Models
+{
+    public class Presupuesto
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+        public decimal MontoPlaneado { get; set; }
+        public decimal MontoGastado { get; set; } 
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public string Estado { get; set; }
+    }
+}
