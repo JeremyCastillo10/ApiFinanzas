d34a54b143de7b3b76d828e7d2ff945de6e4b2c5
jeremy castillo taveras <jeremycastillo901@gmail.com>
2025-01-02 14:32:47 -0400

    Creando modelo Categoria

--- ApiFinanzas/Models/Categoria.cs
+++ ApiFinanzas/Models/Categoria.cs
@@ -1,7 +1,10 @@
-﻿namespace ApiFinanzas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiFinanzas.Models
 {
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
         public string Nombre { get; set; } 
         public string Tipo { get; set; }
